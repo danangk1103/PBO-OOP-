6e6a61bbced5fbec89231b16e851478960a5e92b
danangk23 <89757116+danangk1103@users.noreply.github.com>
2022-08-28 12:37:17 +0700

    Add files via upload

---
+++ MyProject1/src/myproject1/MyMainClass.java
@@ -0,0 +1,23 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package myproject1;
+
+/**
+ *
+ * @author danang
+ */
+public class MyMainClass {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {  
+        System.out.print("Hello World");
+        System.out.println("Hello Java");
+        System.out.println("Java is fun");
+// TODO code application logic here
+    }
+    
+}
---
+++ MyProject2/src/myproject2/MyProject2.java
@@ -0,0 +1,27 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package myproject2;
+
+/**
+ *
+ * @author danang
+ */
+public class MyProject2 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("DDDDDD            AAAA        NNN    NNN        AAAA         NNN    NNN        GGGGGGG");
+        System.out.println("DDDDDDDD        AAAAAAAA      NNNN   NNN      AAAAAAAAA      NNNN   NNN      GGG");
+        System.out.println("DDD    DDD     AAA    AAA     NNNNN  NNN     AAA     AAA     NNNNN  NNN     GGG    ");
+        System.out.println("DDD    DDD     AAA    AAA     NNNNNNNNNN     AAA     AAA     NNNNNNNNNN     GGG GGGGGG");
+        System.out.println("DDD    DDD     AAAAAAAAAA     NNN  NNNNN     AAAAAAAAAAA     NNN  NNNNN     GGG     GG");
+        System.out.println("DDDDDDDD       AAA    AAA     NNN   NNNN     AAA     AAA     NNN   NNNN      GGG    GG");
+        System.out.println("DDDDDD         AAA    AAA     NNN    NNN     AAA     AAA     NNN    NNN        GGGGGGG");
+        // TODO code application logic here
+    }
+    
+}
